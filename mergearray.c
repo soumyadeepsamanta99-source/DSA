67c519b2c0c91a3341d780a32dc6014ffe5a39c2
soumyadeepsamanta99-source <soumyadeepsamanta99@gmail.com>
2026-09-03 22:30:51 +0530

    Add files via upload

---
+++ mergearray.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void merge(int arr[], int p, int q, int r)
+{
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for(int i = 0; i < n1; i++)
+    {
+        L[i] = arr[p + i];
+    }
+
+    for(int j = 0; j < n2; j++)
+    {
+        M[j] = arr[q + 1 + j];
+    }
+
+    int i, j, k;
+
+    i = 0;
+    j = 0;
+    k = p;
+
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 4, 6, 1, 3, 5};
+    int n = 6;
+
+    merge(arr, 0, 2, 5);
+
+    printf("Merged array: ");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
